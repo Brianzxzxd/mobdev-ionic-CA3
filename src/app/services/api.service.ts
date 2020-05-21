ef54013dbbf9c29e013b5c5de8ef403ed1d50e49
Brianzxzxd <qwe362155675@qq.com>
2020-05-21 22:38:53 +0000

    heroku

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,35 +8,35 @@ export class ApiService {
 
   constructor(private http:HttpClient) { }
    getEpisodes() {
-    return this.http.get('https://breakingbadapi.com/api/episodes');
+    return this.http.get('https://fierce-plateau-35840.herokuapp.com//api/episodes');
   }
  /**GET a Episode by ID from breakingbad api website */
   getEpisode(id) {
-    return this.http.get(`https://breakingbadapi.com/api/episodes/${id}`);
+    return this.http.get(`https://fierce-plateau-35840.herokuapp.com//api/episodes/${id}`);
   }
 /**GET characters from breakingbad api website*/
   getCharacters() {
-        return this.http.get('https://www.breakingbadapi.com/api/characters');
+        return this.http.get('https://fierce-plateau-35840.herokuapp.com//api/characters');
     }
 /**GET characters by ID from breakingbad api website*/
     getCharacter(id) {
-        return this.http.get(`https://www.breakingbadapi.com/api/characters/${id}`);
+        return this.http.get(`https://fierce-plateau-35840.herokuapp.com//api/characters/${id}`);
     }
     
     /**GET quotes reletad to episodes from breakingbad api website*/
     getQuotes() {
-        return this.http.get('https://www.breakingbadapi.com/api/quotes');
+        return this.http.get('https://fierce-plateau-35840.herokuapp.com//api/quotes');
     }
     /**GET quotes by ID reletad to episodes from breakingbad api website*/
     getQuote(id) {
-        return this.http.get(`https://www.breakingbadapi.com/api/quotes/${id}`);
+        return this.http.get(`https://fierce-plateau-35840.herokuapp.com//api/quotes/${id}`);
     }
 /**GET numbers of deaths reletad to a character from breakingbad api website*/
    getDeaths() {
-        return this.http.get(`https://www.breakingbadapi.com/api/death-count`);
+        return this.http.get(`https://fierce-plateau-35840.herokuapp.com//api/death-count`);
     }
     /**get killers of deaths from breakingbad api website */
     getKillers() {
-        return this.http.get(`https://www.breakingbadapi.com/api/deaths`);
+        return this.http.get(`https://fierce-plateau-35840.herokuapp.com//api/deaths`);
     }
 }
